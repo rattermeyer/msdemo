f04f60a39563f961beab13c8da0e80da8a9a0bc9
Richard Attermeyer <richard.attermeyer@gmail.com>
2016-09-17 22:20:50 +0200

    updated

--- microservices-demo/user/src/test/java/com/example/DemoApplicationTests.java
+++ microservices-demo/user/src/test/java/com/example/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.opitzconsulting.demo.ms;
+package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BenutzerApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
