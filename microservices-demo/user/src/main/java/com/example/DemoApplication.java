678d9271efba6620a85b693eba189fc22514444c
Richard Attermeyer <richard.attermeyer@gmail.com>
2016-09-23 09:11:03 +0200

    added deployment samples for jar and container

--- microservices-demo/user/src/main/java/com/example/DemoApplication.java
+++ microservices-demo/user/src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,34 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
+    @Value("${greeting}")
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return greeting;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
